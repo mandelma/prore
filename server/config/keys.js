79d22ab5cb7607451ef01d220b150c42837b4375
Marko <mandlimarko@gmail.com>
2023-02-26 13:44:51 +0200

    .gitignore test

--- server/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    //  mongoDB: 'mongodb://localhost:27017/finder'
-    mongoDB: 'mongodb://127.0.0.1/finder'
-    // 'mongodb+srv://mama:a13ma23x@cluster0.w0z1j.mongodb.net/tiler?retryWrites=true&w=majority'
-}
