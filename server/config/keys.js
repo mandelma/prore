075d64090e558cbdc98f5977f29e5e1ca9a643af
Marko <mandlimarko@gmail.com>
2023-02-26 14:19:06 +0200

    test sent dev to master

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //  mongoDB: 'mongodb://localhost:27017/finder'
+    mongoDB: 'mongodb://127.0.0.1/finder'
+    // 'mongodb+srv://mama:a13ma23x@cluster0.w0z1j.mongodb.net/tiler?retryWrites=true&w=majority'
+}
